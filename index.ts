b889a6dd528541c9eb7a2ad1a436826344250afd
Nicolas Even <n-e@users.noreply.github.com>
2018-02-28 22:34:42 +0100

    Fix static and view paths when the app isn't launched from the project folder

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 // Entry Point for the totoz server
 
 import express = require('express')
+import path = require('path')
 import router from './routes'
+
+// Misc. functions
+
+// Makes an absolute path from a path relative to the project folder
+const absPath = (relPath: string) => path.join(__dirname, relPath)
 
 // Environment variables
 const port = +(process.env['PORT'] || 3000)
@@ -10,8 +16,9 @@ const hostname = process.env['HOSTNAME'] || '127.0.0.1'
 const app = express()
 
 app.set('view engine','pug')
+app.set('views',absPath('views'))
 
-app.use(express.static('static'))
+app.use(express.static(absPath('static')))
 app.use('/', router)
 
 app.listen(port,hostname)
