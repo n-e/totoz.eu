aafeade4b7bd93b26e71392ce5e459b93202ca27
Nicolas Even <n-e@users.noreply.github.com>
2018-03-04 15:44:11 +0100

    Serve default images if NOIMAGES is set, add request time logging in development mode

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@
 
 import express = require('express')
 import path = require('path')
+import morgan = require('morgan')
 import router from './routes'
 
 // Misc. functions
@@ -10,8 +11,11 @@ import router from './routes'
 const absPath = (relPath: string) => path.join(__dirname, relPath)
 
 // Environment variables
+//   NOIMAGES : if true, serve a default image for the totozes. Useful for a
+//              dev. environment without the full totoz set.
 const port = +(process.env['PORT'] || 3000)
 const hostname = process.env['HOSTNAME'] || '127.0.0.1'
+const noimages = process.env['NOIMAGES'] ? true : false
 
 // Setup and run app
 const app = express()
@@ -19,7 +23,9 @@ const app = express()
 app.set('view engine','pug')
 app.set('views',absPath('views'))
 
+if(noimages) app.get(/\.gif$/,(req,res)=>res.sendFile(absPath('static/uxam.gif')))
 app.use(express.static(absPath('static')))
+if(app.get('env')=='development') app.use(morgan('tiny'))
 app.use('/', router)
 
 app.listen(port,hostname)
