b165e0c4736aadb3a987662ec49e171f45da77ee
Nicolas Even <n-e@users.noreply.github.com>
2018-03-07 19:48:02 +0100

    Cancel old find-as-you-type request when starting a new one (avoids unnecessary rerenders and problems when responses arrive out-of-order)
    fixes #15

--- static/index.js
+++ static/index.js
@@ -1,16 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
     const totozes = document.getElementsByClassName('totozes')[0]
+    let currentRequest = null
 
     document.getElementById('query').oninput = e => {
         const query = encodeURIComponent(e.target.value)
 
         window.history.replaceState(undefined,'',(query.length > 0) ? '/?q='+query : '/')
         
-        window.fetch('/?tlonly=1&q=' + query)
-            .then(r => r.text())
-            .then(txt => {
-                totozes.innerHTML = txt
-            })
+        if (currentRequest)
+            currentRequest.abort()
+
+        currentRequest = new XMLHttpRequest()
+        currentRequest.addEventListener('load', loadListener)
+        currentRequest.open('GET','/?tlonly=1&q=' + query)
+        currentRequest.send()
+        function loadListener() {
+            totozes.innerHTML = this.responseText
+        }
     }
 })
