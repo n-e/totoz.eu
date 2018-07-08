2a2fff1017b248714eb5c01f03a1fe0f3f1891de
Nicolas Even <n-e@users.noreply.github.com>
2018-07-08 13:30:42 +0200

    Don't try to add events to the query field if it doesn't exist

--- static/index.js
+++ static/index.js
@@ -3,20 +3,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const totozes = document.getElementsByClassName('totozes')[0]
     let currentRequest = null
 
-    document.getElementById('query').oninput = e => {
-        const query = encodeURIComponent(e.target.value)
+    const q = document.getElementById('query')
+    if (q)
+        q.oninput = e => {
+            const query = encodeURIComponent(e.target.value)
 
-        window.history.replaceState(undefined,'',(query.length > 0) ? '/?q='+query : '/')
-        
-        if (currentRequest)
-            currentRequest.abort()
+            window.history.replaceState(undefined,'',(query.length > 0) ? '/?q='+query : '/')
+            
+            if (currentRequest)
+                currentRequest.abort()
 
-        currentRequest = new XMLHttpRequest()
-        currentRequest.addEventListener('load', loadListener)
-        currentRequest.open('GET','/?tlonly=1&q=' + query)
-        currentRequest.send()
-        function loadListener() {
-            totozes.innerHTML = this.responseText
+            currentRequest = new XMLHttpRequest()
+            currentRequest.addEventListener('load', loadListener)
+            currentRequest.open('GET','/?tlonly=1&q=' + query)
+            currentRequest.send()
+            function loadListener() {
+                totozes.innerHTML = this.responseText
+            }
         }
-    }
 })
