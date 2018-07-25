213a04369f39fe0f388faef239b5c9fe07236bf2
Nicolas Even <n-e@users.noreply.github.com>
2018-07-25 18:22:41 +0200

    Fix links that pointed to beta instead of prod

--- routes/common.ts
+++ routes/common.ts
@@ -4,7 +4,7 @@ const router = express.Router()
 
 // Common route whose values is used in layout.pug
 
-router.use('/',(req,res,next) => {
+router.use('/', (req, res, next) => {
     const force_nsfw = req.query.force_nsfw === '1' // for debugging on localhost
 
     if (force_nsfw)
@@ -13,14 +13,15 @@ router.use('/',(req,res,next) => {
         res.locals.sfw = ! (req.headers.host || '').match(/nsfw.*\.totoz\.eu$/)
 
     if ((req.headers.host || '').match(/totoz.eu$/)) {
-        res.locals.sfw_url = 'https://beta.totoz.eu' +  req.url
-        res.locals.nsfw_url = 'https://nsfw.beta.totoz.eu' + req.url
+        res.locals.sfw_url = 'https://totoz.eu' +  req.url
+        res.locals.nsfw_url = 'https://nsfw.totoz.eu' + req.url
     } else {
-        const chr = req.url.match(/\?/) ? '&' : '?' // Crude but good enough for debugging
-        res.locals.sfw_url = req.url.replace(/.force_nsfw=1/,'')
+        // Crude but good enough for debugging
+        const chr = req.url.match(/\?/) ? '&' : '?'
+        res.locals.sfw_url = req.url.replace(/.force_nsfw=1/, '')
         res.locals.nsfw_url = req.url + chr + 'force_nsfw=1'
     }
-    
+
     next()
 })
 
