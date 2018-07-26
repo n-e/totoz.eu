43df2ee08144db86540fc4cf228c0b2641f4a30f
Nicolas Even <n-e@users.noreply.github.com>
2018-07-26 11:31:14 +0200

    Have the sfw/nsfw link send you to the search results and not the homepage

--- static/index.js
+++ static/index.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
     const totozes = document.getElementsByClassName('totozes')[0]
+    const sfw_link = document.getElementsByClassName('sfw_status')[0]
+    const isNsfw = sfw_link.classList.contains('nsfw')
+
     let currentRequest = null
 
     const q = document.getElementById('query')
@@ -8,8 +11,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
         q.oninput = e => {
             const query = encodeURIComponent(e.target.value)
 
-            window.history.replaceState(undefined,'',(query.length > 0) ? '/?q='+query : '/')
-            
+            const new_url = (query.length > 0) ? '/?q='+query : '/'
+
+            // update the link to the nsfw site
+            sfw_link.setAttribute(
+                'href',
+                (isNsfw ? 'https://totoz.eu' : 'https://nsfw.totoz.eu') + new_url)
+
+            // set the new url
+            window.history.replaceState(undefined,'',new_url)
+
+            // launch search
             if (currentRequest)
                 currentRequest.abort()
 
