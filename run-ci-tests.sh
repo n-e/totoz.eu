ac848e0642e7c517edbdf8ff949950bcfad24a56
Nicolas Even <nicolas@even.li>
2026-03-03 21:28:47 +0100

    Add CI Tests

---
+++ run-ci-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+docker compose up -d
+npm ci
+npm run build
+./create-test-db.sh
+DATABASE_URL=psql://totoz:example@localhost/totoz_test npm start &
+npx playwright install chromium
+npx playwright test e2e/
